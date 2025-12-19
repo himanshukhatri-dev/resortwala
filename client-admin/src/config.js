653382d64a6da74fcb51a3a0007c47925d72f175
Himanshu <himanshu.temp@media.net>
2025-12-19 19:05:09 +0530

    removed https

--- client-admin/src/config.js
+++ client-admin/src/config.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = import.meta.env.PROD
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || (import.meta.env.PROD
     ? 'http://stagingapi.resortwala.com/api'
-    : '/api'; // Use proxy in dev
+    : '/api'); // Use proxy in dev
